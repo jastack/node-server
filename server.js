9dd2b5a59aba87ed7eef5092e5f02a332cf81b54
James Stack <james.a.stack@gmail.com>
2017-11-07 16:31:21 -0800

    add git ignore

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + '/public'));
-
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
@@ -22,9 +20,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('./maintain.hbs');
-});
+// Maintenance function
+// app.use((req, res, next) => {
+//   res.render('./maintain.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
